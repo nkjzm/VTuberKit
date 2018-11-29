04e94ce47af687429250246b80c6cca825812164
Nakaji Kohki <boomerang.cube@gmail.com>
2018-11-30 07:55:31 +0900

    Add vtuberkit scripts

---
+++ Assets/VTuberKit/Scripts/GazeController.cs
@@ -0,0 +1,59 @@
+﻿using Live2D.Cubism.Core;
+using UnityEngine;
+
+namespace nkjzm.VTuberKit
+{
+    /// <summary>
+    /// 目線の追従を行うクラス
+    /// </summary>
+    public class GazeController : MonoBehaviour
+    {
+        [SerializeField]
+        Transform Anchor = null;
+        Vector3 centerOnScreen;
+        void Start()
+        {
+            centerOnScreen = Camera.main.WorldToScreenPoint(Anchor.position);
+        }
+        void LateUpdate()
+        {
+            var mousePos = Input.mousePosition - centerOnScreen;
+            UpdateRotate(new Vector3(mousePos.x, mousePos.y, 0) * 0.2f);
+        }
+
+        Vector3 currentRotateion = Vector3.zero;
+        Vector3 eulerVelocity = Vector3.zero;
+
+        [SerializeField]
+        CubismParameter HeadAngleX = null, HeadAngleY = null, HeadAngleZ = null;
+        [SerializeField]
+        CubismParameter EyeBallX = null, EyeBallY = null;
+        [SerializeField]
+        float EaseTime = 0.2f;
+        [SerializeField]
+        float EyeBallXRate = 0.05f;
+        [SerializeField]
+        float EyeBallYRate = 0.02f;
+        [SerializeField]
+        bool ReversedGazing = false;
+        void UpdateRotate(Vector3 targetEulerAngle)
+        {
+            currentRotateion = Vector3.SmoothDamp(currentRotateion, targetEulerAngle, ref eulerVelocity, EaseTime);
+            // 頭の角度
+            SetParameter(HeadAngleX, currentRotateion.x);
+            SetParameter(HeadAngleY, currentRotateion.y);
+            SetParameter(HeadAngleZ, currentRotateion.z);
+            // 眼球の向き
+            SetParameter(EyeBallX, currentRotateion.x * EyeBallXRate * (ReversedGazing ? -1 : 1));
+            SetParameter(EyeBallY, currentRotateion.y * EyeBallYRate * (ReversedGazing ? -1 : 1));
+        }
+
+        void SetParameter(CubismParameter parameter, float value)
+        {
+            if (parameter != null)
+            {
+                parameter.Value = Mathf.Clamp(value, parameter.MinimumValue, parameter.MaximumValue);
+            }
+        }
+    }
+}
---
+++ Assets/VTuberKit/Scripts/SimpleLipSyncer.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using Live2D.Cubism.Core;
+
+namespace VTuberKit
+{
+    /// <summary>
+    /// 口パクを行うクラス
+    /// </summary>
+    [RequireComponent(typeof(AudioSource))]
+    public class SimpleLipSyncer : MonoBehaviour
+    {
+        AudioSource audioSource = null;
+
+        [SerializeField]
+        CubismParameter MouthOpenParameter = null;
+
+        float velocity = 0.0f;
+        float currentVolume = 0.0f;
+
+        [SerializeField]
+        float Power = 20f;
+
+        [SerializeField, Range(0f, 1f)]
+        float Threshold = 0.1f;
+
+        void Start()
+        {
+            // 空の Audio Sourceを取得
+            audioSource = GetComponent<AudioSource>();
+
+            // Audio Source の Audio Clip をマイク入力に設定
+            // 引数は、デバイス名（null ならデフォルト）、ループ、何秒取るか、サンプリング周波数
+            audioSource.clip = Microphone.Start(null, false, 300, 44100);
+            // マイクが Ready になるまで待機（一瞬）
+            while (Microphone.GetPosition(null) <= 0) { }
+            // 再生開始（録った先から再生、スピーカーから出力するとハウリングします）
+            audioSource.Play();
+        }
+
+        private void LateUpdate()
+        {
+            float targetVolume = GetAveragedVolume() * Power;
+            targetVolume = targetVolume < Threshold ? 0 : targetVolume;
+            currentVolume = Mathf.SmoothDamp(currentVolume, targetVolume, ref velocity, 0.05f);
+
+            if (MouthOpenParameter == null)
+            {
+                Debug.LogError("MouthOpenParameterが設定されていません");
+                return;
+            }
+            // CubismParameterの更新はLateUpdate()内で行う必要がある点に注意
+            MouthOpenParameter.Value = Mathf.Clamp01(currentVolume);
+        }
+
+        float GetAveragedVolume()
+        {
+            float[] data = new float[256];
+            float a = 0;
+            audioSource.GetOutputData(data, 0);
+            foreach (float s in data)
+            {
+                a += Mathf.Abs(s);
+            }
+            return a / 255.0f;
+        }
+    }
+}
